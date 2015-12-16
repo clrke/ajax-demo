1718e76454a80e4c4179781101eb393a0cc6598b
Clarke Benedict Plumo <clarke.plumo@gmail.com>
2015-12-16 10:51:38 +0800

    Add tasks get

--- tasks.js
+++ tasks.js
@@ -1,7 +1,23 @@
 PAGES_INITIALIZERS.push(function() {
+    var page = $('.tasks.page'),
+        tasksList = page.find('.tasks.list'),
+        taskTemplate = tasksList.find('.task.template');
+
     API_CALL('get', 'tasks')
-    .success(function(data) {
-        console.log(data);
+    .success(function(response) {
+        var data = response.data;
+
+        data.forEach(function(task) {
+            var newTask = taskTemplate.clone();
+
+            newTask.removeClass('template');
+
+            newTask.find('.name').text(task.name);
+            newTask.find('.description').text(task.description);
+            newTask.find('.estimated-time').text(task.estimated_time);
+
+            tasksList.append(newTask);
+        });
     })
     .error(function(data) {
         console.log(data);
